865eb8b6f88e2adf10853438e9e36f8bc5368a41
AzuleneG <pengjun.guo@outlook.com>
2024-07-20 13:11:04 +0800

    add file

---
+++ R/map_ukbfgsea.R
@@ -0,0 +1,38 @@
+#' Apply Gene Set Enrichment Analysis to Multiple Phenotypes
+#'
+#' This function applies fast preranked gene set enrichment analysis (FGSEA) to multiple phenotypes using a provided gene set. It is designed to handle large-scale datasets efficiently.
+#' @param input_geneset A list containing one gene set for enrichment analysis. The element of the list should be a character vector of gene Ensembl IDs.
+#' @param multiple_phenotypes A data frame containing multiple phenotypes from Genebass.
+#' @param ... Additional arguments passed to `ukbfgsea`.
+#' @return A data frame containing the results of `ukbfgsea` for each phenotype annotation. This includes enrichment analysis outcomes using two different ranking metrics, and, if specified, enrichment plots.
+#' @export
+#' @importFrom dplyr across ungroup
+#' @importFrom utils data
+#' @examples
+#' \dontrun{
+#'   library(tidyverse)
+#'   library(ukbFGSEA)
+#'   asd_output <- map_ukbfgsea(input_geneset = ASD185, multiple_phenotypes = genebass_test_data)
+#' }
+#' @name map_ukbfgsea
+
+utils::globalVariables(names = c("Pvalue", "n_cases", "category", "annotation", "data"), package = "ukbFGSEA",add = F)
+
+map_ukbfgsea <- function(input_geneset, multiple_phenotypes, ...) {
+  multiple_phenotypes <- multiple_phenotypes %>%
+    dplyr::mutate(Pvalue = ifelse(Pvalue == 0, 5e-321, Pvalue))
+
+  genebass_by_phenotype <- multiple_phenotypes %>%
+    dplyr::group_by(across(n_cases:category), annotation) %>%
+    tidyr::nest()
+
+  output <- genebass_by_phenotype %>%
+    dplyr::mutate(output = purrr::map(
+      data,
+      ~ ukbfgsea(input_geneset = input_geneset, ukb_genebass = .x)
+    )) %>%
+    dplyr::select(-data)
+
+  output %>% ungroup()
+
+}
---
+++ R/tidy_map.R
@@ -0,0 +1,34 @@
+#' Tidy the Output of `map_ukbfgsea`
+#'
+#' This function processes the raw output from `map_ukbfgsea()` and organizes it into a tidy format. It splits the output into distinct columns for each phenotype and each ranking method, providing a clear and accessible summary of the FGSEA results.
+#'
+#' @param input A data frame containing the raw results from `map_ukbfgsea()`.
+#' @return A tidy data frame with separate columns for each phenotype and ranking method.
+#' @details This function is designed to simplify the interpretation of FGSEA results by restructuring the output into a user-friendly format. It ensures that each phenotype and ranking method is clearly delineated, making it easier to analyze and visualize the results.
+#' @examples
+#' \dontrun{
+#'   library(tidyverse)
+#'   library(ukbFGSEA)
+#'   asd_output <- map_ukbfgsea(input_geneset = ASD185, multiple_phenotypes = genebass_test_data)
+#'   asd_output_tidy <- tidy_map(asd_output)
+#' }
+#' @export
+#' @importFrom dplyr select
+#' @importFrom utils data
+#' @name tidy_map
+utils::globalVariables(names = c(".", "beta_pathway", "sign_pathway"), package = "ukbFGSEA",add = F)
+
+tidy_map <- function(input) {
+  beta <- purrr::map_dfr(input$output, ~ .x$beta_results %>% as_tibble) %>%
+    stats::setNames(paste0("beta_", names(.)))
+
+  sign <- purrr::map_dfr(input$output, ~ .x$sign_results %>% as_tibble) %>%
+    stats::setNames(paste0("sign_", names(.)))
+
+  output <- dplyr::bind_cols(input %>% select(-output), beta) %>%
+    dplyr::bind_cols(sign)
+
+  output %>%
+    dplyr::select(-beta_pathway, -sign_pathway)
+
+}
